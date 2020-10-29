e5bc1082c49a7710efd11375a5d28f4a9d433856
NoahPidding <72891523+NoahPidding@users.noreply.github.com>
2020-10-29 12:38:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import projects #projects definitions are placed in different file
+import data #projects definitions are placed in different file
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template
@@ -8,17 +8,21 @@ app = Flask(__name__)
 #connects default URL of server to render home.html
 @app.route('/')
 def home_route():
-  return render_template("home.html", projects=projects.setup())
+  return render_template("home.html", projects=data.setup())
 
 #connects /hello path of server to render hello.html
 @app.route('/hello/')
-def hello_rooute():
-  return render_template("hello.html", projects=projects.setup())
+def hello_route():
+  return render_template("hello.html", projects=data.setup())
 
 #connects /flask path of server to render flask.html
 @app.route('/flask/')
 def flask_route():
-  return render_template("flask.html", projects=projects.setup())
+  return render_template("flask.html", projects=data.setup())
+
+@app.route('/playlist/')
+def playlist_route():
+  return render_template("playlist.html", datalist=data.playlist())
 
 if __name__ == "__main__":
   #runs the application on the repl development server
