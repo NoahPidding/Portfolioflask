607c726c0459f5430fa978c9b01fd3aaad8c719b
D-rito <57206018+D-rito@users.noreply.github.com>
2020-10-29 12:57:22 -0700

    Test Commit

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-import data #projects definitions are placed in different file
+import data  # projects definitions are placed in different file
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template
-#create a Flask instance
+# create a Flask instance
 app = Flask(__name__)
 
-#connects default URL of server to render home.html
+# connects default URL of server to render home.html
+
+
 @app.route('/')
 def home_route():
-  return render_template("home.html", projects=data.setup())
+    return render_template("home.html", projects=data.setup())
 
-#connects /hello path of server to render hello.html
+# connects /hello path of server to render hello.html
+
+
 @app.route('/hello/')
 def hello_route():
-  return render_template("hello.html", projects=data.setup())
+    return render_template("hello.html", projects=data.setup())
 
-#connects /flask path of server to render flask.html
+# connects /flask path of server to render flask.html
+
+
 @app.route('/flask/')
 def flask_route():
-  return render_template("flask.html", projects=data.setup())
+    return render_template("flask.html", projects=data.setup())
+
 
 @app.route('/playlist/')
 def playlist_route():
-  return render_template("playlist.html", datalist=data.playlist())
+    return render_template("playlist.html", datalist=data.playlist())
+
 
 if __name__ == "__main__":
-  #runs the application on the repl development server
-  app.run(port='3000', host='127.0.0.1')
+    # runs the application on the repl development server
+    app.run(port='3000', host='127.0.0.1')
